f4203444a46617ffef4f54b151ca88d55995df89
Norman <norman_cade32@yahoo.com>
2021-09-30 16:00:40 -0400

    Complete Info Section

---
+++ src/data/infoData.js
@@ -0,0 +1,26 @@
+import ImageOne from "../images/house-5.jpg";
+import ImageTwo from "../images/house-6.jpg";
+
+export const InfoData = {
+  heading: "Explore our beautiful homes",
+  paragraphOne:
+    "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Hic provident cupiditate illum debitis dicta vitae, iure praesentium, quia sed doloribus harum quasi culpa veritatis? Repudiandae totam labore reprehenderit vitae explicabo.",
+  paragraphTwo:
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos, reiciendis consectetur. Animi porro optio pariatur. Enim possimus et ad totam!",
+  buttonLabel: "View Homes",
+  image: ImageOne,
+  reverse: false,
+  delay: 100,
+};
+
+export const InfoDataTwo = {
+  heading: "Modern Designs",
+  paragraphOne:
+    "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Hic provident cupiditate illum debitis dicta vitae, iure praesentium, quia sed doloribus harum quasi.",
+  paragraphTwo:
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos, reiciendis consectetur. Animi porro optio pariatur. Enim possimus et ad totam! Eos, reiciendis consectetur. Animi porro optio pariatur. Enim possimus et ad totam!",
+  buttonLabel: "View Homes",
+  image: ImageTwo,
+  reverse: false,
+  delay: 300,
+};
