28423112a6dba7673d4792f6950367a11e4f8846
Norman <norman_cade32@yahoo.com>
2021-09-29 23:22:30 -0400

    Buttons and images created for hero section slider

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from "react";
+import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 import GlobalStyle from "./globalStyles";
+import { SliderData } from "./data/SliderData";
 
 function App() {
   return (
     <div className="App">
       <GlobalStyle />
       <Navbar />
+      <Hero slides={SliderData} />
     </div>
   );
 }
---
+++ src/data/SliderData.js
@@ -0,0 +1,39 @@
+import imageOne from "../images/house-1.jpg";
+import imageTwo from "../images/house-2.jpg";
+import imageThree from "../images/house-3.jpg";
+import imageFour from "../images/house-4.jpg";
+
+export const SliderData = [
+  {
+    title: "Luxury Villa in Bali, Indonesia",
+    price: "$4,280,000",
+    path: "/homes",
+    label: "View Home",
+    image: imageFour,
+    alt: "House",
+  },
+  {
+    title: "House in San Jose, Costa Rica",
+    price: "$2,860,000",
+    path: "/homes",
+    label: "View Home",
+    image: imageOne,
+    alt: "House",
+  },
+  {
+    title: "House in Calabasas, California",
+    price: "$8,420,000",
+    path: "/homes",
+    label: "View Home",
+    image: imageThree,
+    alt: "House",
+  },
+  {
+    title: "House in Houston, Texas",
+    price: "$6,660,000",
+    path: "/homes",
+    label: "View Home",
+    image: imageTwo,
+    alt: "House",
+  },
+];
